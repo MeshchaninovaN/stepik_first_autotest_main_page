050e6810133bce68d753dfbc9f1713bc336f4094
Meshchaninova Natalia <wildviolet12@gmail.com>
2022-09-28 12:51:18 +0400

    изменение файлов base- и main-

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,5 @@ class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-	
-	def open(self):
+    def open(self):
 	    self.browser.get(self.url)
